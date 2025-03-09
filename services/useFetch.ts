81295eb8686d6e0651639d4b40a77b9cc7ceebdd
seid <seyasmoke49@gmail.com>
2025-03-09 18:51:50 +0300

    pass as prop some data rem-ift

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const useFetch = <T>(fetchFunction: ()=> Promise <T>, autoFetch = true)=>{
+export const useFetch = <T>(fetchFunction: ()=> Promise <T>, autoFetch = true)=>{
     const [data,setData] = useState<T | null>(null);
     const [loading,setLoading] = useState(false);
     const [error,setError] = useState<Error | null>(null);
@@ -30,5 +30,5 @@ const useFetch = <T>(fetchFunction: ()=> Promise <T>, autoFetch = true)=>{
             fetchData();
         }
     },[]);
-    return {data ,loading, reset,refetch:fetchData};
+    return {data ,loading, reset,refetch:fetchData,error};
 }
