6a94ad308120de421216228e564a521441cf99f9
seid <seyasmoke49@gmail.com>
2025-03-09 16:27:47 +0300

    --

---
+++ sevices/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Njk5OWVjODU0ZTI1ODAyM2YwMjkxZGI3YWYzMTNkMyIsIm5iZiI6MTcxNTc5MjAxOS4yMzIsInN1YiI6IjY2NDRlODkzYTUwN2U3MGU2MGNmOTJkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6hw7uXOS98mJsI0Rakgvhf9wc22bs4fdMrr75jzF6o0'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
